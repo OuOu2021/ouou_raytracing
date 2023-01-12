0e6ae32dc314ddd7815d218412407cca4d625cea
OuOu2021 <1113117424@qq.com>
2023-01-12 10:55:14 +0800

    * add fuzz value for Metal * implement fuzz effect

--- src/main.rs
+++ src/main.rs
@@ -76,8 +76,8 @@ fn main() -> MyResult {
 
     let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let material_center = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
-    let material_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
-    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
+    let material_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
     world.add(Box::new(Sphere::new(
         Point3::new(0., -100.5, -1.),
